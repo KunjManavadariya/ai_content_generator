a47ec39476f2a73991b81f205e433886b57e1133
kunj manavadariya <kunj@socialpilot.co>
2025-03-25 12:24:48 +0530

    Added URL from env

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy API requests to your backend
-      "/v1": {
-        target: "http://localhost:3000", // Your backend server address
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(env.VITE_API_URL);
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/v1": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
