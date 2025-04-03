3e9bf1903a3d23a1c54bd3513f49cc4e83cef6f3
kunj manavadariya <kunj@socialpilot.co>
2025-04-03 17:27:17 +0530

    Prompt changes

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
     topHowManyPosts: 10,
     generateHowManyPosts: 3,
     aiModelPlatform: "bedrock",
-    aiPrompt: `You are an expert social media content creator. I will provide you with my top highest-performing posts from Instagram, along with top-performing posts from various competitors. Your task is to analyze them and generate 3 new high-performing posts based on the themes, tone, and engagement patterns that have worked best in terms of engagement, likes, comments, shares, etc from my posts and my competitors posts.
+    aiPrompt: `You are an expert social media content creator. I will provide you past data of my page's top highest-performing posts from Instagram, along with top-performing posts from various competitors. Your task is to analyze them and generate 3 new high-performing posts based on the themes, tone, and engagement patterns that have worked best in terms of engagement, likes, comments, shares, etc from my posts and my competitors posts.
 
 Instructions:
 • Only provide the posts (text post or single image post) in a directly usable format.
@@ -146,21 +146,37 @@ Instructions:
   };
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+    if (name === "socialMediaPlatform") {
+      let updatedPrompt = formData.aiPrompt;
+      if (value === "5") {
+        updatedPrompt = updatedPrompt.replace(/Instagram/g, "Facebook");
+      } else if (value === "25") {
+        updatedPrompt = updatedPrompt.replace(/Facebook/g, "Instagram");
+      }
+      setFormData({
+        ...formData,
+        socialMediaPlatform: value,
+        aiPrompt: updatedPrompt,
+      });
+      return;
+    }
     if (name === "aiModelPlatform" && value === "openai") {
       setIsModelDisabled(true);
       setFormData((prevData) => ({
         ...prevData,
+        aiModelPlatform: value,
         imageToTextModel: "gpt-4o",
         textToTextModel: "gpt-4o",
         textToImageModel: "gpt-4o",
       }));
+      return;
     } else {
       setIsModelDisabled(false);
     }
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
   const handleSubmit = async (e: FormEvent) => {
